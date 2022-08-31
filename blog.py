47e142fe5e5200c29230028f5f75a982a950ad18
Carlos <alcala21@gmail.com>
2022-08-31 17:59:36 -0500

    Stage 3/5. Many-to-many relations.

--- blog.py
+++ blog.py
@@ -11,7 +11,6 @@ if os.path.exists(db_name):
     os.remove(db_name)
 
 con = sqlite3.connect(db_name)
-cursor = con.cursor()
 
 # Create tables
 meal_query = """
@@ -29,26 +28,76 @@ ingredient_name text not null unique);
 meas_query = """
 create table if not exists measures (
 measure_id integer primary key,
-measure_name text unique
-);
+measure_name text unique);
 """
 
-cursor.execute(meal_query)
-cursor.execute(ing_query)
-cursor.execute(meas_query)
+rec_query = """
+create table if not exists recipes (
+recipe_id integer primary key,
+recipe_name text not null,
+recipe_description text);
+"""
+
+ser_query = """
+create table if not exists serve (
+serve_id integer primary key,
+recipe_id integer not null,
+meal_id integer not null,
+foreign key(recipe_id) references recipes(recipe_id),
+foreign key(meal_id) references meals(meal_id));
+"""
+
+con.execute("pragma foreign_keys = on;")
+con.execute(meal_query)
+con.execute(ing_query)
+con.execute(meas_query)
+con.execute(rec_query)
+con.execute(ser_query)
 
 con.commit()
 
-data = {"meals": ("breakfast", "brunch", "lunch", "supper"),
-        "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
-        "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
 
-for table, elements in data.items():
-    sql_query = f"insert into {table} ({table[:-1]}_name)  values (?);"
-    cursor.executemany(sql_query, ((element, ) for element in elements))
+def stage1(_con):
+    data = {"meals": ("breakfast", "brunch", "lunch", "supper"),
+            "ingredients": ("milk", "cacao", "strawberry", "blueberry", "blackberry", "sugar"),
+            "measures": ("ml", "g", "l", "cup", "tbsp", "tsp", "dsp", "")}
 
-con.commit()
-cursor.close()
+    for table, elements in data.items():
+        sql_query = f"insert into {table} ({table[:-1]}_name)  values (?);"
+        _con.executemany(sql_query, ((element, ) for element in elements))
+
+    _con.commit()
+    print("Tables were created!")
+
+
+def stage2(_con):
+    print("Pass the empty recipe name to exit.")
+    while name := input("Recipe name: "):
+        description = input("Recipe description: ")
+        sql_query = "insert into recipes (recipe_name, recipe_description) values (?, ?);"
+        _con.execute(sql_query, (name, description))
+    _con.commit()
+    print("Recipes were added!")
+
+
+def stage3(_con):
+    print("Pass the empty recipe name to exit.")
+    while name := input("Recipe name: "):
+        description = input("Recipe description: ")
+        meals = _con.execute("select * from meals;").fetchall()
+        print(" ".join(f"{meal[0]}) {meal[1]}" for meal in meals))
+        try:
+            nums = [int(n) for n in input("When the dish can be served: ").split(" ")]
+            last_id = _con.execute("insert into recipes(recipe_name, recipe_description) values (?, ?);", (name, description)).lastrowid
+            for num in nums:
+                _con.execute("insert into serve (recipe_id, meal_id) values (?, ?);", (last_id, num))
+        except ValueError:
+            print("Enter integers separated by a space.")
+    _con.commit()
+    print("Recipes were added!")
+
+
+stage1(con)
+stage3(con)
 con.close()
 
-print("Tables were created!")
